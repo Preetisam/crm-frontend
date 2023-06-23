85a9d63218c12891eafaad73e074873404d6f346
Preetisam <preetikamilla@gmail.com>
2023-06-23 23:22:05 +0530

    new update url

--- src/api/tickect.js
+++ src/api/tickect.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const BASE_URL = "https://notification-backend.onrender.com"
+const BASE_URL = "https://crmtest-gnhe.onrender.com"
 
 export async function fetchTickets(){
     return await axios.get(`${BASE_URL}/crm/api/v1/tickets`, {
