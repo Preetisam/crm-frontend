222f9893480744177e7c47640458f4fcdd5a3766
Preetisam <preetikamilla@gmail.com>
2023-06-28 12:48:11 +0530

    updated version

--- src/api/ticket.js
+++ src/api/ticket.js
@@ -1,55 +1,29 @@
 import axios from "axios"
 
-const BASE_URL = "https://crm-backend-z5m4.onrender.com"
+const BASE_URL = "https://backend-crm-yic4.onrender.com"
 
-export async function fetchTicket() {
-  try {
-    const response = await axios.get(`${BASE_URL}/crm/api/v1/tickets`, {
-      headers: {
-        "x-access-token": localStorage.getItem("token"),
-      },
-    });
-    return response.data;
-  } catch (error) {
-    console.log("Error occurred during fetching tickets: ", error.message);
-    throw error;
-  }
-}
 
-export async function createTicketApi(data) {
-  try {
-    const response = await axios.post(
-      `${BASE_URL}/crm/api/v1/tickets`,
-      data,
-      {
+
+export async function fetchTickets(){
+    return await axios.get(`${BASE_URL}/crm/api/v1/tickets`, {
         headers: {
-          "x-access-token": localStorage.getItem("token"),
-        },
-      }
-    );
-    return response.data;
-  } catch (error) {
-    console.log("Error occurred during creating ticket: ", error.message);
-    throw error;
-  }
+            'x-access-token' : localStorage.getItem("token")
+        }
+    })
 }
 
-export async function updateTicketApi(data, id) {
-  try {
-    const response = await axios.post(
-      `${BASE_URL}/crm/api/v1/tickets/${id}`,
-      data,
-      {
-        headers: {
-          "x-access-token": localStorage.getItem("token"),
-        },
-      }
-    );
-    return response.data;
-  } catch (error) {
-    console.log("Error occurred during updating ticket: ", error.message);
-    throw error;
-  }
+export async function createTicketApi(data){
+    return await axios.post(`${BASE_URL}/crm/api/v1/tickets`, data, {
+        headers:{
+            'x-access-token' : localStorage.getItem("token")
+        }
+    })
 }
 
-  
+export async function updateTicketApi(ticketId, data){
+    return await axios.put(`${BASE_URL}/crm/api/v1/tickets/${ticketId}`, data, {
+        headers:{
+            'x-access-token' : localStorage.getItem("token")
+        }
+    })
+}
--- src/pages/Coustmer.js
+++ src/pages/Coustmer.js
@@ -1,176 +1,343 @@
 import MaterialTable from "@material-table/core";
 import { useState, useEffect } from "react";
-
+import { fetchTickets, createTicketApi, updateTicketApi } from "../api/ticket";
 import Sidebar from "../components/Sidebar";
-import { Button, DropdownButton, Modal,  Dropdown} from "react-bootstrap";
-import { fetchTicket,createTicketApi } from "../api/ticket";
+import { Button, Modal, Card } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 function Customer() {
-    const [ticketDetails, setTicketDetails] = useState([]);
-    const [message, setMessage] = useState("");
-    const [createTicketModal, setCreateTicketModal] = useState(false);
-    const [priority, setPriority] = useState("");
-  
-    const columns = [
-      {
-        title: "ID",
-        field: "_id",
-      },
-      {
-        title: "TITLE",
-        field: "title",
-      },
-      {
-        title: "DESCRIPTION",
-        field: "description",
-      },
-      {
-        title: "ASSIGNEE",
-        field: "assignee",
-      },
-      {
-        title: "PRIORITY",
-        field: "ticketPriority",
-      },
-      {
-        title: "STATUS",
-        field: "status",
-      },
-    ];
-  
-    useEffect(() => {
-      fetchTicketData();
-    }, []);
-  
-    async function fetchTicketData() {
-      try {
-        const res = await fetchTicket();
-        setTicketDetails(res.data);
-      } catch (err) {
-        console.log(
-          "Error occurred during fetching all tickets: " + err.message
-        );
-        setMessage(err.message);
-      }
-    }
-  
-    const createTicket = (e) => {
-      e.preventDefault();
-  
-      const data = {
-        title: e.target.elements.title.value,
-        description: e.target.elements.description.value,
-        priority: priority,
-      };
-      console.log("Data for creating ticket is " + JSON.stringify(data));
-  
-      createTicketApi(data)
-        .then((response) => {
-          setCreateTicketModal(false);
-          setMessage("Ticket created successfully");
-          fetchTicketData();
-        })
-        .catch((err) => {
-          setMessage(err.message);
-        });
-    };
-  
-    return (
-      <div className="bg-light vh-100">
-        <Sidebar />
-        <div className="container-fluid pt-5">
-          <h3 className="text-center text-success">
-            Welcome, {localStorage.getItem("name")}
-          </h3>
-        </div>
-        <p className="text-center text-muted">Take a look at all tickets below</p>
-        <div className="container-fluid pt-5 p-3">
-          <MaterialTable
-            title="Ticket raised by you"
-            columns={columns}
-            data={ticketDetails}
-          />
-          <hr />
-          <p className="text-primary text-center">{message}</p>
-          <h4 className="text-center "> Facing any issues ? Raise a ticket!</h4>
-          <button
-            className="btn btn-lg btn-success form-control"
-            onClick={() => setCreateTicketModal(true)}
-          >
-            Raise ticket
-          </button>
-  
-          {createTicketModal ? (
-            <Modal
-              show={createTicketModal}
-              onHide={() => setCreateTicketModal(false)}
-              centered
-            >
-              <Modal.Header closeButton>Create a new Ticket</Modal.Header>
-              <Modal.Body>
-                <form onSubmit={createTicket}>
-                  <div className="input-group m-1">
-                    <label className="label label-md input-group-text">
-                      Title
-                    </label>
-                    <input
-                      className="form-control"
-                      type="text"
-                      name="title"
-                      required
-                    />
-                  </div>
-  
-                  <div className="input-group m-1">
-                    <label className="label label-md input-group-text">
-                      Description
-                    </label>
-                    <input
-                      className="form-control"
-                      type="text"
-                      name="description"
-                      required
-                    />
-                  </div>
-                  <div className="input-group m-1">
-                    <label className="label label-md input-group-text">
-                      Priority
-                    </label>
-                    <DropdownButton
-                      align="end"
-                      title={priority}
-                      id="priority"
-                      onSelect={(value) => setPriority(value.toString())}
-                      variant="light"
-                    >
-                      <Dropdown.Item eventKey="1" fw-normal="true">1</Dropdown.Item>
-                     <Dropdown.Item eventKey="2" fw-normal="true">2</Dropdown.Item>
-                    <Dropdown.Item eventKey="3" fw-normal="true">3</Dropdown.Item>
-                    <Dropdown.Item eventKey="4" fw-normal="true">4</Dropdown.Item>
+  const [ticketDetails, setTicketDetails] = useState([]);
+  const [message, setMessage] = useState("");
+  const [createTicketModal, setCreateTicketModal] = useState(false);
+  const [updateTicketModal, setUpdateTicketModal] = useState(false);
+  const [currentSelectedTicket, setCurrentSelectedTicket] = useState({});
+  const [openCount, setOpenCount] = useState(0);
+  const [closedCount, setClosedCount] = useState(0);
+  const [inProgressCount, setInProgressCount] = useState(0);
+  const [blockedCount, setBlockedCount] = useState(0);
 
-                    </DropdownButton>
-                  </div>
-  
-                  <div className="d-flex justify-content-end">
-                    <Button
-                      onClick={() => setCreateTicketModal(false)}
-                      variant="secondary"
-                      className="m-1"
-                    >
-                      Cancel
-                    </Button>
-                    <Button type="submit" variant="success" className="m-1">
-                      {" "}
-                      Create
-                    </Button>
-                  </div>
-                </form>
-              </Modal.Body>
-            </Modal>
-          ) : null}
-        </div>
-      </div>
-    );
+  const updateCurrentSelectedTicket = (data) => setCurrentSelectedTicket(data);
+
+  const columns = [
+    {
+      title: "ID",
+      field: "_id",
+    },
+    {
+      title: "TITLE",
+      field: "title",
+    },
+    {
+      title: "DESCRIPTION",
+      field: "description",
+    },
+    {
+      title: "ASSIGNEE",
+      field: "assignee",
+    },
+    {
+      title: "PRIORITY",
+      field: "ticketPriority",
+    },
+    {
+      title: "STATUS",
+      field: "status",
+    },
+  ];
+
+  useEffect(() => {
+    fetchTicketData();
+    fetchTicketCounts(); // Fetch ticket counts when the component mounts
+  }, []);
+
+  async function fetchTicketData() {
+    try {
+      const res = await fetchTickets();
+      setTicketDetails(res.data);
+    } catch (err) {
+      console.log("Error occurred during fetching all tickets: ", err);
+      setMessage(err.message);
+    }
   }
-  
-  export default Customer;
+
+  async function fetchTicketCounts() {
+    try {
+      const res = await fetchTickets();
+      const ticketData = res.data;
+
+      let openCount = 0;
+      let closedCount = 0;
+      let inProgressCount = 0;
+      let blockedCount = 0;
+
+      ticketData.forEach((ticket) => {
+        if (ticket.status === "OPEN") {
+          openCount++;
+        } else if (ticket.status === "CLOSED") {
+          closedCount++;
+        } else if (ticket.status === "IN_PROGRESS") {
+          inProgressCount++;
+        } else if (ticket.status === "BLOCKED") {
+          blockedCount++;
+        }
+      });
+
+      setOpenCount(openCount);
+      setClosedCount(closedCount);
+      setInProgressCount(inProgressCount);
+      setBlockedCount(blockedCount);
+    } catch (error) {
+      console.log("Error occurred while fetching ticket counts:", error);
+    }
+  }
+
+  const createTicket = (e) => {
+    e.preventDefault();
+
+    const data = {
+      title: e.target.title.value,
+      description: e.target.description.value,
+      priority: e.target.priority.value,
+    };
+
+    createTicketApi(data)
+      .then((response) => {
+        setCreateTicketModal(false);
+        setMessage("Ticket created successfully");
+        fetchTicketData();
+      })
+      .catch((err) => {
+        setMessage(err.message);
+      });
+  };
+
+  const editTicket = (ticketDetail) => {
+    const ticket = {
+      _id: ticketDetail._id,
+      title: ticketDetail.title,
+      description: ticketDetail.description,
+      assignee: ticketDetail.assignee,
+      reporter: ticketDetail.reporter,
+      priority: ticketDetail.ticketPriority,
+      status: ticketDetail.status,
+    };
+
+    setCurrentSelectedTicket(ticket);
+    setUpdateTicketModal(true);
+    console.log("Selected ticket details are:", JSON.stringify(currentSelectedTicket));
+  };
+
+  const onTicketUpdate = (e) => {
+    const { name, value } = e.target;
+    setCurrentSelectedTicket((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const updateTicket = (e) => {
+    e.preventDefault();
+
+    updateTicketApi(currentSelectedTicket._id, currentSelectedTicket)
+      .then((res) => {
+        setUpdateTicketModal(false);
+        setMessage("Ticket updated successfully");
+        fetchTicketData();
+      })
+      .catch((err) => {
+        setMessage(err.message);
+      });
+  };
+  const navigate = useNavigate();
+
+  const logoutFn = () => {
+      localStorage.clear();
+      navigate("/");
+  };
+
+  return (
+    <div className="bg-light vh-100">
+      <Sidebar />
+
+      <div className="container-fluid pt-5">
+        <h3 className="text-center text-success">Welcome, {localStorage.getItem("name")}</h3> 
+        <br/>
+        <div className="d-flex justify-content-end"onClick={logoutFn }>
+        <Button variant="danger">Logout</Button>
+      </div>
+      <br/>
+      </div>
+      
+
+      <div className="d-flex justify-content-around">
+        <Card style={{ width: "18rem" }}>
+          <Card.Body>
+            <Card.Title>OPEN</Card.Title>
+            <Card.Text>{openCount} tickets</Card.Text>
+          </Card.Body>
+        </Card>
+
+        <Card style={{ width: "18rem" }}>
+          <Card.Body>
+            <Card.Title>CLOSED</Card.Title>
+            <Card.Text>{closedCount} tickets</Card.Text>
+          </Card.Body>
+        </Card>
+
+        <Card style={{ width: "18rem" }}>
+          <Card.Body>
+            <Card.Title>IN PROGRESS</Card.Title>
+            <Card.Text>{inProgressCount} tickets</Card.Text>
+          </Card.Body>
+        </Card>
+
+        <Card style={{ width: "18rem" }}>
+          <Card.Body>
+            <Card.Title>BLOCKED</Card.Title>
+            <Card.Text>{blockedCount} tickets</Card.Text>
+          </Card.Body>
+        </Card>
+      </div>
+     
+      
+      <p className="text-center text-muted">Take a look at all your tickets below!</p>
+      <div className="container-fluid p-5 p-3">
+        <MaterialTable
+          onRowClick={(event, rowData) => editTicket(rowData)}
+          title="Tickets raised by you"
+          columns={columns}
+          data={ticketDetails}
+        />
+        <hr />
+        <p className="text-primary text-center">{message}</p>
+        <h4 className="text-center">Facing any issues? Raise a ticket!</h4>
+        <button
+          className="btn btn-lg btn-success form-control"
+          onClick={() => setCreateTicketModal(true)}
+        >
+          Raise ticket
+        </button>
+
+        {createTicketModal && (
+          <Modal
+            show={createTicketModal}
+            onHide={() => setCreateTicketModal(false)}
+            centered
+            backdrop="static"
+          >
+            <Modal.Header closeButton>Create a new ticket</Modal.Header>
+            <Modal.Body>
+              <form onSubmit={createTicket}>
+                <div className="input-group m-1">
+                  <label className="label label-md input-group-text">Title</label>
+                  <input className="form-control" type="text" name="title" required />
+                </div>
+
+                <div className="input-group m-1">
+                  <label className="label label-md input-group-text">Description</label>
+                  <input className="form-control" type="text" name="description" required />
+                </div>
+
+                <div className="input-group m-1">
+                  <label className="label label-md input-group-text">Priority</label>
+                  <select name="priority" id="priority">
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                  </select>
+                </div>
+
+                <div className="d-flex justify-content-end">
+                  <Button onClick={() => setCreateTicketModal(false)} variant="secondary" className="m-1">
+                    Cancel
+                  </Button>
+
+                  <Button type="submit" variant="success" className="m-1">
+                    Create
+                  </Button>
+                </div>
+              </form>
+            </Modal.Body>
+          </Modal>
+        )}
+
+        {updateTicketModal && (
+          <Modal show={updateTicketModal} centered onHide={() => setUpdateTicketModal(false)}>
+            <Modal.Header>Update ticket</Modal.Header>
+            <Modal.Body>
+              <form onSubmit={updateTicket}>
+                <h5 className="card-subtitle text-success lead">ID: {currentSelectedTicket._id}</h5>
+
+                <div className="input-group m-1">
+                  <label className="label label-md input-group-text">Title</label>
+                  <input
+                    className="form-control"
+                    type="text"
+                    name="title"
+                    value={currentSelectedTicket.title}
+                    disabled
+                  />
+                </div>
+
+                <div className="input-group m-1">
+                  <label className="label label-md input-group-text">Assignee</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="assignee"
+                    value={currentSelectedTicket.assignee}
+                    disabled
+                  />
+                </div>
+
+                <div className="input-group m-1">
+                  <label className="label label-md input-group-text">Priority</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="ticketPriority"
+                    value={currentSelectedTicket.priority}
+                    disabled
+                  />
+                </div>
+
+                <div className="input-group m-1">
+                  <label className="label label-md input-group-text">Description</label>
+                  <textarea
+                    type="text"
+                    rows="3"
+                    name="description"
+                    value={currentSelectedTicket.description}
+                    onChange={onTicketUpdate}
+                  />
+                </div>
+
+                <div>
+                  <label>Status</label>
+                  <select onChange={onTicketUpdate} name="status" value={currentSelectedTicket.status}>
+                    <option value="OPEN">OPEN</option>
+                    <option value="CLOSED">CLOSED</option>
+                    <option value="IN PROGRESS">IN PROGRESS</option>
+                    <option value="BLOCKED">BLOCKED</option>
+                  </select>
+                </div>
+
+                <div className="d-flex justify-content-end">
+                  <Button variant="secondary" className="m-1" onClick={() => setUpdateTicketModal(false)}>
+                    Cancel
+                  </Button>
+                  <Button variant="success" className="m-1" type="submit">
+                    Update
+                  </Button>
+                </div>
+              </form>
+            </Modal.Body>
+          </Modal>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Customer;
